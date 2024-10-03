f05ebb9a4875e7c396ea47db678fb617dab72c5e
Lachlan Leone <starfluxgames@gmail.com>
2024-10-03 15:43:55 +1000

    Initial Commit

---
+++ Customs/Claws/LocationPointer.cs
@@ -0,0 +1,15 @@
+﻿using CraneCosmetics.Customs.Types;
+using CraneCosmetics.Enums;
+using KitchenLib.Utils;
+using UnityEngine;
+
+namespace UtilityCraneCosmetics.Customs.Claws
+{
+    public class LocationPointer : CustomCraneCosmetic
+    {
+        public override string UniqueNameID => "PumpkinHat";
+        public override GameObject Prefab => Mod.Bundle.LoadAsset<GameObject>("LocationPointer").AssignMaterialsByNames();
+        public override CraneCosmeticType CosmeticType => CraneCosmeticType.Claw;
+        public override bool HideOriginal => false;
+    }
+}
---
+++ Mod.cs
@@ -0,0 +1,41 @@
+using KitchenLib;
+using KitchenLib.Logging;
+using KitchenLib.Logging.Exceptions;
+using KitchenMods;
+using System.Linq;
+using System.Reflection;
+using KitchenLib.Interfaces;
+using UnityEngine;
+using KitchenLogger = KitchenLib.Logging.KitchenLogger;
+
+namespace UtilityCraneCosmetics
+{
+    public class Mod : BaseMod, IModSystem, IAutoRegisterAll
+    {
+        public const string MOD_GUID = "com.starfluxgames.utilitycranecosmetics";
+        public const string MOD_NAME = "Utility Crane Cosmetics";
+        public const string MOD_VERSION = "0.1.0";
+        public const string MOD_AUTHOR = "StarFluxGames";
+        public const string MOD_GAMEVERSION = ">=1.2.0";
+
+        internal static AssetBundle Bundle;
+        internal static KitchenLogger Logger;
+
+        public Mod() : base(MOD_GUID, MOD_NAME, MOD_AUTHOR, MOD_VERSION, MOD_GAMEVERSION, Assembly.GetExecutingAssembly()) { }
+
+        protected override void OnInitialise()
+        {
+            Logger.LogWarning($"{MOD_GUID} v{MOD_VERSION} in use!");
+        }
+
+        protected override void OnUpdate()
+        {
+        }
+
+        protected override void OnPostActivate(KitchenMods.Mod mod)
+        {
+            Bundle = mod.GetPacks<AssetBundleModPack>().SelectMany(e => e.AssetBundles).FirstOrDefault() ?? throw new MissingAssetBundleException(MOD_GUID);
+            Logger = InitLogger();
+        }
+    }
+}
